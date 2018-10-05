59ea2149f616258e07c6c9f4c945e36bff910cb8
paulabarreto <paulavilaca@gmail.com>
2018-10-05 22:09:06 +0000

    refactored and fixed classNames

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const randomColor = require('randomcolor');
-const uuidv1 = require('uuid/v1');
+const uuid = require('uuid/v1');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -27,13 +27,37 @@ wss.on('connection', (ws) => {
   const color = {type: "colors", color: randomColor()};
   const usersOnline = parseInt(wss.clients.size);
   ws.send(JSON.stringify(color));
-  // wss.broadcast({id: uuidv1(), color: color, type: "colors"});
-  wss.broadcast({id: uuidv1(), usersOnline: usersOnline, type: "usersOnline"});
+  wss.broadcast({id: uuid(), usersOnline: usersOnline, type: "usersOnline"});
 
 
   ws.on('message', function incoming(message) {
+    //Assigning id to each message
+    const id = uuid();
+    var incomingMessage = {...JSON.parse(message), id: id}
+
+    //changing message type from post to incoming
+    switch(incomingMessage.type){
+      case "postMessage":
+        incomingMessage.type = "incomingMessage";
+        break;
+      case "postNotification":
+        incomingMessage.type = "incomingNotification";
+        break;
+    }
+
+    //handling images
+
+    // https://img.hi5messages.com/en/funny/35.jpg
+    
+      if (/(.* .*)?.*\.jpg$/.test(incomingMessage.content)) {
+        // let space = typedMsg.indexOf(" ");
+        // typedMsg = typedMsg.slice(space, typedMsg.length + 1);
+        // incomingMessage.content = typedMsg.slice(0, space);
+        incomingMessage.type = "image";
+      }
+
     wss.clients.forEach(function each(client) {
-        client.send(message);
+      client.send(JSON.stringify(incomingMessage));
     });
     ws.on('close', () => console.log('Client disconnected'));
   });
